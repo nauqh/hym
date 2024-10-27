9728bd776eca99c13ee433c2780278acd4687f78
Nauqh <hodominhquan.work@gmail.com>
2024-10-27 20:15:01 +1100

    feat(requirements.txt): add dmg proportion

--- app/main.py
+++ app/main.py
@@ -244,13 +244,4 @@ with r:
     st.markdown(f"`Title`: {champion['title']}")
     st.markdown(f"`Role`: {', '.join(champion['tags'])}")
 
-st.markdown("""### ⭐ Star the project on Github <iframe src="https://ghbtns.com/github-btn.html?user=nauqh&repo=porobot&type=star&count=true" width="150" height="20" title="GitHub"></iframe>""", unsafe_allow_html=True)
-
-hide_streamlit_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style>
-"""
-
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown("""### ⭐ Star the project on Github <iframe src="https://ghbtns.com/github-btn.html?user=nauqh&type=follow&count=true&size=large" frameborder="0" scrolling="0" width="230" height="30" title="GitHub"></iframe>""", unsafe_allow_html=True)
