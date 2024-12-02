37758f7a8d3a0e41e667eb6472ff08ef86e1a415
Nauqh <hodominhquan.work@gmail.com>
2024-12-02 16:01:43 +0700

    feat(M12v2): add M21 v2

--- app/utils/riot.py
+++ app/utils/riot.py
@@ -63,7 +63,7 @@ def load_data(puuids: set) -> pd.DataFrame:
     df1 = pd.read_csv('app/data/100games_new.csv')
     df2 = pd.read_csv('app/data/100games.csv')
     df = pd.concat([df1, df2])
-    df = df.drop_duplicates(subset=['matchId'])
+    df = df.drop_duplicates()
 
     def get_focus_matches(df: pd.DataFrame, puuids: set) -> pd.DataFrame:
         """
